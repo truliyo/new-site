007a83a40ac61b6033996770d7ce73b67cc0a33d
Sonu Thakur <sonu88ggn@gmail.com>
2025-10-04 13:51:32 +0530

    changes

--- src/app/components/Layout/Header/Logo/index.tsx
+++ src/app/components/Layout/Header/Logo/index.tsx
@@ -1,20 +1,21 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
 const Logo: React.FC = () => {
   return (
-    <Link href='/' className='text-3xl font-semibold'>
-      <div className="relative w-[150px] h-[40px] md:w-[200px] md:h-[50px] lg:w-[250px] lg:h-[60px]">
-      <Image
-        src="/images/hero/new-logo-removebg-preview.png"
-        alt="logo"
-        fill
-        className="object-contain"
-        priority
-      />
-    </div>
+    <Link href="/" className="text-3xl font-semibold">
+      <div className="relative w-[180px] h-[50px] md:w-[240px] md:h-[65px] lg:w-[300px] lg:h-[80px]">
+        <Image
+          src="/images/hero/new-logo-removebg-preview.png"
+          alt="logo"
+          width={180}
+          height={50}
+          className="object-contain"
+          priority
+        />
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
